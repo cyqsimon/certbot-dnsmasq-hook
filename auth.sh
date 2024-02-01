8263b074c1dcce29303fa91011fea72f7728ce9a
cyqsimon <28627918+cyqsimon@users.noreply.github.com>
2024-02-01 18:34:10 +0800

    Append instead of overwrite

--- auth.sh
+++ auth.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-echo "txt-record=_acme-challenge.$CERTBOT_DOMAIN,\"$CERTBOT_VALIDATION\"" > /etc/dnsmasq.d/certbot-acme-challenge.conf
+echo "txt-record=_acme-challenge.$CERTBOT_DOMAIN,\"$CERTBOT_VALIDATION\"" \
+    >> /etc/dnsmasq.d/certbot-acme-challenge.conf
 
 systemctl restart dnsmasq.service
 sleep 3
